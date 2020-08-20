9852b66fe49c6ca865a2ca279f0a545f5644b29b
lkobetz <larissa.kobetz@gmail.com>
2020-08-20 10:58:08 -0400

    added ts interface

--- index.ts
+++ index.ts
@@ -7,6 +7,20 @@ import axios from 'axios'
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1'
 
+interface Todo {
+  id: number
+  title: string
+  completed: boolean
+}
+
 axios.get(url).then((response) => {
-  console.log(response.data)
+  const todo = response.data as Todo
+  const id = todo.id
+  const title = todo.title
+  const completed = todo.completed
+  console.log(`
+    The id is ${id}
+    The title is ${title}
+    It is completed- ${completed}
+  `)
 })
